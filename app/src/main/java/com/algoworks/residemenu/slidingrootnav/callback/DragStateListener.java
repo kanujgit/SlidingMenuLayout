381be319312c7c114850aeae9b2ccae51c5f393e
anuj <anuj.kumar@algoworks.com>
2019-02-01 20:00:14 +0530

    first commit

---
+++ app/src/main/java/com/algoworks/residemenu/slidingrootnav/callback/DragStateListener.java
@@ -0,0 +1,12 @@
+package com.algoworks.residemenu.slidingrootnav.callback;
+
+/**
+ * Created by yarolegovich on 25.03.2017.
+ */
+
+public interface DragStateListener {
+
+    void onDragStart();
+
+    void onDragEnd(boolean isMenuOpened);
+}
